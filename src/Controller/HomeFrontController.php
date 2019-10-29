c40aea117154bb1dd9cb75aeeed883300b1dc78e
Franck <patouillardfranck3@gmail.com>
2019-10-29 20:16:24 +0100

    EDIT THIS FILES FOR FRONT OFFICE AND BACK OFFICE

--- src/Controller/HomeFrontController.php
+++ src/Controller/HomeFrontController.php
@@ -5,14 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class HomeFrontController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('FRONT/home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
