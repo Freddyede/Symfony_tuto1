5b2888587a8b6fcf0454b2ecc58a0d804bcaed78
Franck <patouillardfranck3@gmail.com>
2019-10-30 14:40:58 +0100

    Add security

---
+++ src/Migrations/Version20191030131748.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191030131748 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user ADD is_active TINYINT(1) NOT NULL, DROP mail, CHANGE password password VARCHAR(255) NOT NULL, CHANGE name username VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user ADD mail LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci, DROP is_active, CHANGE password password LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci, CHANGE username name VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci');
+    }
+}
